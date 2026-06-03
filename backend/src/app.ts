a124ad42cff9e637612ce4b8e4b0a5b0100f944d
Andre <andre.ramos15@fatec.sp.gov.br>
2026-06-03 11:30:54 -0300

    feat: Update app.ts with all routes including auth

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,8 @@ import dotenv from "dotenv"
 import alunoRoutes from './routers/Aluno.routes'
 import professorRoutes from './routers/Professor.routes'
 import disciplinaRouter from './routers/Disciplina.routes'
+import boletimRouter from './routers/Boletim.routes'
+import authRouter from './routers/Auth.routes'
 
 dotenv.config()
 
@@ -12,9 +14,16 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// Auth routes (sem middleware de autenticação)
+app.use("/api/auth", authRouter)
+
+// Protected routes (com autenticação)
 app.use("/api/aluno", alunoRoutes)
 app.use("/api/professor", professorRoutes)
 app.use("/api/disciplina", disciplinaRouter)
+app.use("/api/boletim", boletimRouter)
+
+// 404 handler
 app.use("/", (_: Request, res: Response) => res.sendStatus(404))
 
 export default app
